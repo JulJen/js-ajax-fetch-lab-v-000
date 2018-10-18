4576b4fad559578ad35565398f922326b7e4f48b
Jul Jen <juliajen.prog@gmail.com>
2018-10-18 04:29:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,14 +9,14 @@ function forkRepo() {
   //use fetch to fork it!
 
   fetch(`https://api.github.com/repos/${repo}/forks`, {
-  method: 'POST',
-  body: JSON.stringify('postData'),
-  headers: {
-  Authorization: `token ${getToken()}`
-  }
-})
-.then(res => res.json())
-.then(json => console.log(json));
+    method: 'POST',
+    body: JSON.stringify('postData'),
+    headers: {
+    Authorization: `token ${getToken()}`
+    }
+  })
+  .then(res => res.json())
+  .then(json => console.log(json));
 }
 
 function showResults(json) {
@@ -25,6 +25,16 @@ function showResults(json) {
 
 function createIssue() {
   //use this function to create an issue based on the values input in index.html
+  const issueTitle = document.getElementById('title').value
+  const issueBody = document.getElementById('body').value
+  const postData = { title: issueTitle, body: issueBody }
+  fetch(`${baseApi}repos/${fork}/issues`, {
+    method: 'post',
+    headers: {
+      'Authorization': `token ${getToken()}`
+    },
+    body: JSON.stringify(postData)
+  }).then(resp => getIssues())
 }
 
 function getIssues() {
